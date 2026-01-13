bad27925a5829e3312eed83634e4abfbaf4f36d1
Juanma <juanma151@gmail.com>
2026-01-13 12:07:56 +0100

    toWire finished

--- src/main/java/soltest/MySolution.java
+++ src/main/java/soltest/MySolution.java
@@ -1,22 +1,54 @@
 package soltest;
 
-import blch.framework.annotations.Solution;
-import blch.framework.datamodel.VValue;
-import blch.framework.datamodel.WireValue;
-import blch.framework.external.SolutionBase;
+import static blch.framework.dsl.WireDsl.obj;
+import static blch.framework.dsl.WireDsl.v;
+import static soltest.ArrObj.arr;
+import static soltest.MapObj.map;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import blch.framework.annotations.Solution;
+import blch.framework.external.SolutionBase;
 
 @Solution
 public class MySolution extends SolutionBase {
 
-  @Override
   public Object execute(Object... args) {
-    Map<String, WireValue> mymap = new LinkedHashMap<>();
-    mymap.put("name", new VValue("Alice"));
-
-    return mymap;
+      return execute6();
   }
+  
+  public Object execute6(Object... args) {
+      return arr(1,"aa",3,4,5).addf("000","001").addl(false,false).rf().rl().set(2,arr()
+	      .addl(1,2,3,4,5,6)
+	      );
+  }
+  
+  public Object execute5(Object... args) {
+      return map("name","Eve")
+	      .kv("age", 22)
+	      .kv("email", "eve@example.com")
+	      .kv("map", map()
+		      .kv("a",1)
+		      .kv("b",2)
+		      
+		      );
+  }
+  
+  public Object execute4(Object... args) {
+      return map("name","Alice");
+  }  
+    
+  public Object execute3(Object... args) {
+    return obj("name", v("Alice"));
+  }
+  
+  public Object execute2(Object... args) {
+      Map<String, String> mymap = new LinkedHashMap<>();
+      mymap.put("name", "Alice");
+
+      return mymap;
+    }
 
   public Object execute1(Object... args) {
     Map<String, Object> mymap = new LinkedHashMap<>();
